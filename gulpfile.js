c3ca106238d60f0f3fdf873e255602777c4ed78f
sbnair <shai567@rediffmail.com>
2020-09-28 00:50:57 +0530

    Check in the changes done 13

--- gulpfile.js
+++ gulpfile.js
@@ -563,6 +563,17 @@ gulp.task('show:env', function (cb) {
   }
 });
 
+var gulp        = require('gulp');
+var deploy      = require('gulp-gh-pages');
+
+/**
+ * Push build to gh-pages
+ */
+gulp.task('deploy', function () {
+  return gulp.src("./dist/**/*")
+    .pipe(deploy())
+});
+
 
 /**
  * @_EXPOSE
